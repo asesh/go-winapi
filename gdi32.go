093106cef42f541796ce8b9617056d59f718e99d
Chui-Wen Chiu <sisimi.pchome@gmail.com>
2013-06-01 22:54:03 +0800

    add api
    GetROP2, SetROP2

--- gdi32.go
+++ gdi32.go
@@ -1840,3 +1840,42 @@ func CreatePen(fnPenStyle, nWidth int32, crColor COLORREF)HPEN{
         
 	return HPEN(ret)
 }
+
+/* Binary raster ops */
+const (
+    R2_BLACK            = 1   /*  0       */
+    R2_NOTMERGEPEN      = 2   /* DPon     */
+    R2_MASKNOTPEN       = 3   /* DPna     */
+    R2_NOTCOPYPEN       = 4   /* PN       */
+    R2_MASKPENNOT       = 5   /* PDna     */
+    R2_NOT              = 6   /* Dn       */
+    R2_XORPEN           = 7   /* DPx      */
+    R2_NOTMASKPEN       = 8   /* DPan     */
+    R2_MASKPEN          = 9   /* DPa      */
+    R2_NOTXORPEN        = 10  /* DPxn     */
+    R2_NOP              = 11  /* D        */
+    R2_MERGENOTPEN      = 12  /* DPno     */
+    R2_COPYPEN          = 13  /* P        */
+    R2_MERGEPENNOT      = 14  /* PDno     */
+    R2_MERGEPEN         = 15  /* DPo      */
+    R2_WHITE            = 16  /*  1       */
+    R2_LAST             = 16
+)
+
+func SetROP2(hdc HDC, fnDrawMode int32) int32{
+    ret, _, _ := syscall.Syscall(MustGetProcAddress(libgdi32, "SetROP2"), 2,
+		uintptr(hdc),
+		uintptr(fnDrawMode),
+		0)
+        
+	return int32(ret)
+}
+
+func GetROP2(hdc HDC)int32 {
+    ret, _, _ := syscall.Syscall(MustGetProcAddress(libgdi32, "GetROP2"), 1,
+		uintptr(hdc),
+		0,
+		0)
+        
+	return int32(ret)
+}
